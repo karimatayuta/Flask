23ac5382a5eadc58d8436f984ee20f2de748c976
狩俣佑太 <ignorantbeginner@gmail.com>
2018-11-15 22:50:34 +0900

    first_run

--- github_tool.py
+++ github_tool.py
@@ -1,1 +1,22 @@
-print('Hello git!')
+# GitHub 自動化
+import subprocess
+
+print('please input file')
+text = input()
+
+# ファイルをインデックス追加（コマンド入力）
+index = 'git add {}'.format(text)
+subprocess.call(index.split())
+
+print('please input comment')
+comment = input()
+
+# ファイルを登録（コミット）
+comment = 'git commit -m {}'.format(comment)
+subprocess.call(comment.split())
+
+# データの送信
+push = 'git push origin master'
+subprocess.call(push.split())
+
+print('file_name:'+ text +' commit:'+ push +'')
