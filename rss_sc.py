daf26a391a284c26603b95eeb91431c837e9c3db
狩俣佑太 <ignorantbeginner@gmail.com>
2018-11-16 00:30:33 +0900

    first_commit

---
+++ rss_sc.py
@@ -0,0 +1,9 @@
+# RSSのスクレイピング 
+import feedparser
+
+d = feedparser.parse('http://b.hatena.ne.jp/hotentry/it.rss')
+
+for entry in d.entries:
+    print(entry.title)
+    print(entry.link)
+    print('')
