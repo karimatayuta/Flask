9f82c61cd638088b170f0d2ec599e1b841837dad
狩俣佑太 <ignorantbeginner@gmail.com>
2019-04-12 01:34:04 +0900

    delete file

--- rss_sc.py
+++
@@ -1,9 +0,0 @@
-# RSSのスクレイピング 
-import feedparser
-
-d = feedparser.parse('http://b.hatena.ne.jp/hotentry/it.rss')
-
-for entry in d.entries:
-    print(entry.title)
-    print(entry.link)
-    print('')
